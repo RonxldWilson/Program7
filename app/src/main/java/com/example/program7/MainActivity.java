c3fec2c5bdea6fe9174584d8becf2db8371cd6ad
RonxldWilson <ronaldwilson00000@gmail.com>
2021-11-14 02:31:25 +0530

    Initial commit

---
+++ app/src/main/java/com/example/program7/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.program7;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+//Async Tasks Program 
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
